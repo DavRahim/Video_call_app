f4337b658142f17b31f56e66682775314c6b16bf
Abdur Rahim <mohammadabdurrahim1966@gmail.com>
2025-08-24 21:30:12 +0600

    video call app finish

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "imljzgcuelzzzncfzlnc.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "mjhhlswqrjzrhgeozzev.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
